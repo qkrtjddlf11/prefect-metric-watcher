4b08a08c7d54f8bcf2ed5094a061447f2b841147
박성일 <sungil.park@bagseong-il-ui-Macmini.local>
2023-11-27 17:07:42 +0900

    Fixed docker build script

--- docker-build-dev.sh
+++ docker-build-dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker stop `docker ps | grep -v CONTAINER | awk '{print $1}'`
-docker rm `docker ps | grep -v CONTAINER | awk '{print $1}'`
+docker rm `docker ps -a | grep -v CONTAINER | awk '{print $1}'`
 
 docker build --force-rm -f Dockerfile -t prefect-metric-watcher:v0.0.4 .
